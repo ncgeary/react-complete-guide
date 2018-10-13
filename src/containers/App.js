c861b5bb14dc2a9c1262eb65d8c96e1fdaee45cf
Nicholas Geary <ncgeary@buffalo.edu>
2018-10-13 11:49:45 -0400

    10/13/2018

--- src/containers/App.js
+++ src/containers/App.js
@@ -58,11 +58,13 @@ class App extends Component {
 
     return (
       <div className={classes.App}>
+
         <Cockpit
           appTitle={this.props.title}
           showPersons={this.state.showPersons}
           persons={this.state.persons}
           clicked={this.togglePersonsHandler} />
+          
         {persons}
       </div>
     );
